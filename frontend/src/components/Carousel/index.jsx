a521536042a4d417639a1754012f9c8a1286af93
TomDvpmt <tbsuiviactu@gmail.com>
2023-03-28 12:18:20 +0200

    Carousel OK (but not infinite loop)

--- frontend/src/components/Carousel/index.jsx
+++ frontend/src/components/Carousel/index.jsx
@@ -1,35 +1,78 @@
-import styles from "./Carousel.module.css";
+import { useEffect, useState } from "react";
+
 import chevronLeft from "../../assets/chevron/chevron_left.png";
 import chevronRight from "../../assets/chevron/chevron_right.png";
 
-const Carousel = () => {
-    const handleCarouselNavigation = (e) => {
-        console.log(e.target.id === "left" ? "left" : "right");
+import PropTypes from "prop-types";
+
+import styles from "./Carousel.module.css";
+
+const Carousel = ({ images, title }) => {
+    Carousel.propTypes = {
+        images: PropTypes.array,
+        title: PropTypes.string,
     };
 
+    const [carouselPosition, setCarouselPosition] = useState(0);
+
+    const handleCarouselNavigation = (e) => {
+        if (e.target.id === "right") {
+            setCarouselPosition((carouselPosition) =>
+                carouselPosition === images.length - 1
+                    ? 0
+                    : carouselPosition + 1
+            );
+        } else {
+            setCarouselPosition((carouselPosition) =>
+                carouselPosition === 0
+                    ? images.length - 1
+                    : carouselPosition - 1
+            );
+        }
+    };
+
+    useEffect(() => {
+        console.log(carouselPosition);
+        console.log(100 * carouselPosition + "%");
+    }, [carouselPosition, images]);
+
     return (
-        <section className={styles.carousel}>
-            <img
-                id="left"
-                className={`${styles.chevron} ${styles.left}`}
-                src={chevronLeft}
-                alt="Carousel navigation left"
-                onClick={handleCarouselNavigation}
-            />
-            <img
-                id="right"
-                className={`${styles.chevron} ${styles.right}`}
-                src={chevronRight}
-                alt="Carousel navigation right"
-                onClick={handleCarouselNavigation}
-            />
-            <div className={styles.images}>
-                <img
-                    src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-                    alt="mock data"
-                />
-            </div>
-        </section>
+        <>
+            {images && (
+                <section className={styles.carousel}>
+                    {images.length > 1 && (
+                        <img
+                            id="left"
+                            className={`${styles.chevron} ${styles.left}`}
+                            src={chevronLeft}
+                            alt="Carousel navigation left"
+                            onClick={handleCarouselNavigation}
+                        />
+                    )}
+                    {images.length > 1 && (
+                        <img
+                            id="right"
+                            className={`${styles.chevron} ${styles.right}`}
+                            src={chevronRight}
+                            alt="Carousel navigation right"
+                            onClick={handleCarouselNavigation}
+                        />
+                    )}
+
+                    <div className={styles.images}>
+                        {images.map((image, index) => (
+                            <div
+                                key={index}
+                                className={styles.carousel_image}
+                                style={{
+                                    backgroundImage: `url(${image})`,
+                                    transform: `translateX(calc(-100% * ${carouselPosition})`,
+                                }}></div>
+                        ))}
+                    </div>
+                </section>
+            )}
+        </>
     );
 };
 
--- frontend/src/pages/Accomodation/index.jsx
+++ frontend/src/pages/Accomodation/index.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Carousel from "../../components/Carousel";
 
 import styles from "./Accomodation.module.css";
 
 const Accomodation = () => {
+    const id = useParams().id;
+    const [accomodation, setAccomodation] = useState({});
+
+    useEffect(() => {
+        fetch("../logements.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setAccomodation(data.filter((element) => element.id === id)[0]);
+            })
+
+            .catch((error) => console.log(error));
+    }, [id]);
+
     return (
         <main className={styles.accomodation}>
-            <Carousel />
+            <Carousel
+                images={accomodation.pictures}
+                title={accomodation.title}
+            />
+            <section>
+                <h1>{accomodation.title}</h1>
+            </section>
         </main>
     );
 };
